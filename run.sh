f7da5a646bbf910a859d30cd42054502575e4799
Ben Gamari <ben@smart-cactus.org>
2017-11-17 16:32:47 -0500

    Work

--- run.sh
+++ run.sh
@@ -2,12 +2,17 @@
 
 ghc=ghc
 
-$ghc -c Decl.hs
+$ghc -c Extension.hs
 $ghc -c Expr.hs
+$ghc -c Decl.hs
 
 $ghc -c Instances1.hs-boot
 $ghc -c Instances2.hs
-$ghc -c Instances1.hs
 $ghc -c Instances2.hs-boot
+$ghc -c Instances1.hs
+
+$ghc -c Main.hs
 
 objdump -x *.o | grep zdfx
+
+$ghc -o hi.o Extension.o Expr.o Decl.o Instances1.o Instances2.o Main.o
