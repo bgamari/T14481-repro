f2bdb8aad45dd22b3188124e58b26789aa128620
Ben Gamari <ben@smart-cactus.org>
2017-11-17 16:48:26 -0500

    Kill redundant pragmas

--- Instances1.hs
+++ Instances1.hs
@@ -1,8 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Instances1 where
--- Instances2.hs-boot
+++ Instances2.hs-boot
@@ -1,7 +1,4 @@
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Instances2 where
