e82cb636fd5e9cb1d821a82d9c69f6e468c92b33
Ben Gamari <ben@smart-cactus.org>
2017-11-17 16:56:07 -0500

    Add recursion between types

--- Types.hs
+++ Types.hs
@@ -1,6 +1,8 @@
 module Types where
 
+-- Note how these types are mutually recursive
 data Expr p = Var String
             | App (Expr p) (Expr p)
+            | ADecl (Decl p)
 
 data Decl p = Bind String (Expr p)
